4d99f51b44ef40637e34ef185c809a1969fa6f3d
FCCoffice <office@floorcareconcepts.net>
2026-08-05 15:03:28 -0400

    Load approved FCC Ops reference shell styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react';import{createRoot}from'react-dom/client';import FccOpsApp from './FccOpsApp.jsx';import './fccops.css';import './responsive.css';createRoot(document.getElementById('root')).render(<React.StrictMode><FccOpsApp/></React.StrictMode>);
+import React from 'react';import{createRoot}from'react-dom/client';import FccOpsApp from './FccOpsApp.jsx';import './fccops.css';import './responsive.css';import './reference-shell.css';createRoot(document.getElementById('root')).render(<React.StrictMode><FccOpsApp/></React.StrictMode>);
